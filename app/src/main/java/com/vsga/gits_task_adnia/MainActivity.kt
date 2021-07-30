600558f76e48758bc0ff0d516d0dd5fa8e42063e
adniasalsabila <adniasalsabila8@gmail.com>
2021-07-30 21:41:27 +0700

    Task 2

--- app/src/main/java/com/vsga/gits_task_adnia/MainActivity.kt
+++ app/src/main/java/com/vsga/gits_task_adnia/MainActivity.kt
@@ -6,36 +6,40 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.core.view.isVisible
+import java.util.regex.Pattern.compile
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val editText = findViewById<EditText>(R.id.input_number)
+        val emailRegex = compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,20}" +
+                    "\\@" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "\\.id" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+"
+        )
+        val editText = findViewById<EditText>(R.id.input_email)
         val buttonSubmit = findViewById<Button>(R.id.buttonSubmit)
         val textView = findViewById<TextView>(R.id.textView_Result)
 
-        buttonSubmit.setOnClickListener(View.OnClickListener {
-            val number = editText.text.toString().toInt()
+        fun resultMatcher(email:String):Boolean{
+            return emailRegex.matcher(email).matches()
+        }
 
-            if (number % 3 == 0) {
-                textView.text = ("Hello")
-                textView.visibility = View.VISIBLE
+        buttonSubmit.setOnClickListener(View.OnClickListener {
+            val email = editText.text.toString()
+            val emailMatcher = resultMatcher(email)
+            if (emailMatcher==true){
+                textView.text = ("Your Email is Valid")
+            }else{
+                textView.text = ("Your Email is Invalid")
             }
-            if (number % 5 == 0) {
-                textView.text = ("World")
-                textView.visibility = View.VISIBLE
-            }
-            if (number % 3 == 0 && number % 5 == 0) {
-                textView.text = ("Hello World")
-                textView.visibility = View.VISIBLE
-            }
-            if (number % 3 != 0 && number % 5 != 0) {
-                textView.text = ("not divisible by 3 and 5")
-                textView.visibility = View.VISIBLE
-            }
+
         })
     }
 }
