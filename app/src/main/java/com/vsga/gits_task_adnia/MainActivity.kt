d664566aa10e0c56965ce0ec2c5d54267615140f
adniasalsabila <adniasalsabila8@gmail.com>
2021-07-30 20:22:57 +0700

    Task 5

--- app/src/main/java/com/vsga/gits_task_adnia/MainActivity.kt
+++ app/src/main/java/com/vsga/gits_task_adnia/MainActivity.kt
@@ -6,36 +6,33 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editText = findViewById<EditText>(R.id.input_number)
+        val editText = findViewById<EditText>(R.id.input_text)
         val buttonSubmit = findViewById<Button>(R.id.buttonSubmit)
         val textView = findViewById<TextView>(R.id.textView_Result)
 
         buttonSubmit.setOnClickListener(View.OnClickListener {
-            val number = editText.text.toString().toInt()
-
-            if (number % 3 == 0) {
-                textView.text = ("Hello")
-                textView.visibility = View.VISIBLE
-            }
-            if (number % 5 == 0) {
-                textView.text = ("World")
-                textView.visibility = View.VISIBLE
-            }
-            if (number % 3 == 0 && number % 5 == 0) {
-                textView.text = ("Hello World")
-                textView.visibility = View.VISIBLE
-            }
-            if (number % 3 != 0 && number % 5 != 0) {
-                textView.text = ("not divisible by 3 and 5")
-                textView.visibility = View.VISIBLE
+            val text = editText.text.toString().trim()
+            if (isPalindrome(text)) {
+                textView.text =("$text is a True Palindrome")
+            } else {
+                textView.text =("$text is a False Palindrome")
             }
         })
     }
+
+    fun isPalindrome(str: String): Boolean {
+        val stringBuilder = StringBuilder(str)
+
+        val reverseStr = stringBuilder.reverse().toString()
+
+        return str.equals(reverseStr, ignoreCase = true)
+    }
+
+
 }
